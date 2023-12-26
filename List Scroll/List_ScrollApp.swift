181a48525c883eef752e248fb3e9dded0faf75bb
ADJOfficial <ArsalanDaud098@gamil.com>
2023-12-26 17:39:05 +0500

    Initial Commit

---
+++ List Scroll/List_ScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_ScrollApp.swift
+//  List Scroll
+//
+//  Created by ADJ on 26/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct List_ScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
